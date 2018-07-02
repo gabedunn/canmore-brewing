53fee37477c3d7b8bdb9543fcd4d9ab8d0d9f937
Gabriel Dunn <gabe.dunn@shaw.ca>
2018-07-02 14:15:05 -0600

    add some vue config

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  pwa: {
+    themeColor: '#E3592A',
+    msTileColor: '#E3592A'
+  },
+
+  lintOnSave: true,
+  assetsDir: 'assets',
+  productionSourceMap: true,
+  parallel: true,
+
+  css: {
+    sourceMap: true
+  }
+}
