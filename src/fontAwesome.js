12e30a19069d5e6e7f7f996c6cddbd9f55bda664
Gabriel Dunn <gabe.dunn@shaw.ca>
2018-07-07 12:20:24 -0600

    move fontawesome to specific file & add brands icons
    Former-commit-id: 3c81ab41e0ae64c883242517c5e69e4bd4d34f64 [formerly c3becfa8980649ddf7714a7f1536ed707d9525ae] [formerly 246bd03c4d1f82c84a9674e1298ab212bb19f6d8 [formerly 6c43e0bcfee1e9322c6cd5078dc822e33145d187]]
    Former-commit-id: 88db4dc4700fee97e83ce6fbcf8b06531c161a73 [formerly 42086aa0f062216e7f6fcc58cc96e28cb40e7870]
    Former-commit-id: e1e2e8ac47a2bca7cc2688474dacad83f449d9da

---
+++ src/fontAwesome.js
@@ -0,0 +1,5 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
+import { faTwitter, faInstagram, faFacebook } from '@fortawesome/free-brands-svg-icons'
+
+library.add(faChevronDown, faTwitter, faInstagram, faFacebook)
