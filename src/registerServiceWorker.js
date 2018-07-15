c9c111c7903a37fdd6a533bcb232f053b73fe9fe
Gabriel Dunn <gabe.dunn@shaw.ca>
2018-07-15 00:39:28 -0600

    unregister service worker on updated
    Former-commit-id: e8dfce5f504821c721ce244fc6ee5dc9fbfa7939 [formerly 89aebe36fa9101201075b3107b8219e83681d695] [formerly 6b535bfa4487607adbb600b183a93f5e1b82e906 [formerly e58e23c03d830da125f120bc342d2de22b672468]]
    Former-commit-id: de5acbb747057ba8221f998f53185744ddb41b74 [formerly 463342faae05fd44d54a8bd12eea29169961f23d]
    Former-commit-id: 670ff116c76945e546286dcd9ada7afd8a230ca2

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 
-import { register } from 'register-service-worker'
+import { register, unregister } from 'register-service-worker'
 
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
@@ -15,9 +15,7 @@ if (process.env.NODE_ENV === 'production') {
     },
     updated () {
       console.log('New content is available; please refresh.')
-      caches.keys().then(function (names) {
-        for (let name of names) { caches.delete(name) }
-      })
+      unregister()
     },
     offline () {
       console.log('No internet connection found. App is running in offline mode.')
@@ -27,3 +25,5 @@ if (process.env.NODE_ENV === 'production') {
     }
   })
 }
+
+// unregister()
