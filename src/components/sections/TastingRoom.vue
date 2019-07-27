35df85dc37b2de5b026b7c2ee032ab94a8f790be
Gabe Dunn <gabe.dunn@shaw.ca>
2019-07-27 16:20:15 -0600

    edit tasting room text

--- src/components/sections/TastingRoom.vue
+++ src/components/sections/TastingRoom.vue
@@ -4,15 +4,41 @@
     <div class="container mx-auto">
       <div class="mx-40 my-12">
         <div class="tasting-room text-justify text-cbblack-grey font-semibold font-cbsans">
-          <p>
-            We are super proud of our beautiful brewery and love to show it off. Book a tour to learn more about
-            where we came from and how we make our delicious beer.
+          <p class="py-2">
+            Our brewery has a bright and family-friendly tasting room, a retail area and small patio, open 6 days a week
+            and located next to the Safeway on Railway Avenue.
           </p>
-          <p>
-            Regular tours are available every Saturday from 1:30-2:30. Click the button below to reserve your
-            spot. Private
-            tours for 10 or more and private events are also available. Come celebrate with us. For more info
-            please contact.
+          <p class="py-2">
+            In addition to our usual line up of beers, we also serve seasonal brews, pilot brews (our brewer’s latest
+            small batch creations), and our brand new Beer Cocktails. Juice and San Pellegrino are in the self-serve
+            case for designated drivers and youngsters.
+          </p>
+          <p class="py-2">
+            The retail area carries Canmore Brewing branded road cycling kit, mountain bike jerseys, an assortment of
+            tee shirts and flannels, hats, buffs, glassware and a rotating inventory of fun socks and novelty items.
+            Perfect as souvenirs or gifts for your beer-loving friends and family. Everyone looks great in orange!
+          </p>
+          <p class="py-2">
+            Although our brewery does not have a kitchen, we invite you to order a pizza to your table, bring your own
+            picnic or
+            <a
+              v-scroll-to="'#food'"
+              href="#"
+            >
+              click here
+            </a>
+            to check out the tasty snacks we serve in our tasting room, all thoughtfully selected to go perfectly with
+            beer!
+          </p>
+          <p class="py-2">
+            Drop in for a pint, a growler fill or some take away cans. You know you want to!
+          </p>
+
+          <p class="py-2">
+            <a
+              v-scroll-to="'#contact'"
+              href="#"
+            >Click here to view our summer hours</a>.
           </p>
         </div>
         <figure>
