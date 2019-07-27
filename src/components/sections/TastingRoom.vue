2943ef51c6cfa91dc84ab8e2828039cd39f14925
Gabe Dunn <gabe.dunn@shaw.ca>
2019-07-27 15:37:09 -0600

    add basic tasting room section

---
+++ src/components/sections/TastingRoom.vue
@@ -0,0 +1,90 @@
+<template>
+  <section id="tasting-room">
+    <header-title title="Tasting Room" />
+    <div class="container mx-auto">
+      <div class="mx-40 my-12">
+        <div class="tasting-room text-justify text-cbblack-grey font-semibold font-cbsans">
+          <p>
+            We are super proud of our beautiful brewery and love to show it off. Book a tour to learn more about
+            where we came from and how we make our delicious beer.
+          </p>
+          <p>
+            Regular tours are available every Saturday from 1:30-2:30. Click the button below to reserve your
+            spot. Private
+            tours for 10 or more and private events are also available. Come celebrate with us. For more info
+            please contact.
+          </p>
+        </div>
+        <figure>
+          <picture>
+            <source
+              srcset="../../assets/img/tasting_room/landing-background.webp"
+              type="image/webp"
+            >
+            <source
+              srcset="../../assets/img/tasting_room/landing-background.png"
+              type="image/png"
+            >
+            <img
+              alt="Tasting Room"
+              src="../../assets/img/tasting_room/landing-background.png"
+            >
+          </picture>
+        </figure>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+  import HeaderTitle from '../Header'
+
+  export default {
+    name: 'TastingRoom',
+    components: {
+      HeaderTitle
+    },
+    data () {
+      return {
+        title: 'Tasting Room'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  /* noinspection CssInvalidAtRule */
+  @tailwind base;
+  /* noinspection CssInvalidAtRule */
+  @tailwind components;
+  /* noinspection CssInvalidAtRule */
+  @tailwind utilities;
+
+  .tasting-room {
+    font-size: 1rem;
+  }
+
+  @media (min-width: 768px) {
+    .tasting-room {
+      font-size: 1.1rem;
+    }
+  }
+
+  @media (min-width: 1024px) {
+    .tasting-room {
+      font-size: 1.2rem;
+    }
+  }
+
+  @media (min-width: 1280px) {
+    .tasting-room {
+      font-size: 1.4rem;
+    }
+  }
+
+  @media (min-width: 1440px) {
+    .tasting-room {
+      font-size: 1.6rem;
+    }
+  }
+</style>
