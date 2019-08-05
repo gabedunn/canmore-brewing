96f099665be57c60069bc71f046a91e3e4a9253e
Gabe Dunn <gabe.dunn@shaw.ca>
2019-08-05 12:07:26 -0600

    centre containers by default
    Former-commit-id: 1fc01b9322d87c85ba871206f11e2a8100d5c33c [formerly 8cf8f349138200a74fa5d34c9c47aa5b03c6b777] [formerly a3dfb1892dc6d8d5df869d91f370f0b472a168d0 [formerly b0dc7dacddbf43eef9f698c300c8c837bf21d9a7]]
    Former-commit-id: 2704b539507965ad525d26173e6ca215e4b21f12 [formerly f77ee2ce3b161843be69355e6ac46a6931eece20]
    Former-commit-id: 007bac33f954dff32340321e7d8c3b806d4e49b2

--- src/components/sections/Events.vue
+++ src/components/sections/Events.vue
@@ -1,7 +1,7 @@
 <template>
   <section id="events">
     <header-title title="events" />
-    <div class="container mx-auto">
+    <div class="container">
       <div class="mx-40 my-12">
         <div class="tasting-room text-justify text-cbblack-grey font-semibold font-cbsans">
           <p>
