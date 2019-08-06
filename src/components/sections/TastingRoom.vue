cc020737abfeb48b36735f829ed876edb9505687
Gabe Dunn <gabe.dunn@shaw.ca>
2019-08-05 18:05:59 -0600

    add spacing to tasting room page
    Former-commit-id: 1055857b59221646247016cbc454b842af845f76 [formerly c9501eeb86924237be4b722ac3ddb09b7ff0baf9] [formerly e582b693a7be475fea57e312140bf5b20a6fc6ba [formerly d513cd8c168f4276026a7896916cc448106a05c1]]
    Former-commit-id: 50f10914651687279a5c01619f491c7185fc5f76 [formerly 27778e46f26ece3e1191d86180f8c463cc46fe90]
    Former-commit-id: 7dcc1904a4185d3b2386d65491638bd8925287e6

--- src/components/sections/TastingRoom.vue
+++ src/components/sections/TastingRoom.vue
@@ -3,60 +3,65 @@
     <header-title title="Tasting Room" />
     <div class="container">
       <div class="mx-40 my-12">
-        <div class="tasting-room text-justify text-cbblack-grey font-semibold font-cbsans">
-          <p class="py-2">
-            Our brewery has a bright and family-friendly tasting room, a retail area and small patio, open 6 days a week
-            and located next to the Safeway on Railway Avenue.
-          </p>
-          <p class="py-2">
-            In addition to our usual line up of beers, we also serve seasonal brews, pilot brews (our brewer’s latest
-            small batch creations), and our brand new Beer Cocktails. Juice and San Pellegrino are in the self-serve
-            case for designated drivers and youngsters.
-          </p>
-          <p class="py-2">
-            The retail area carries Canmore Brewing branded road cycling kit, mountain bike jerseys, an assortment of
-            tee shirts and flannels, hats, buffs, glassware and a rotating inventory of fun socks and novelty items.
-            Perfect as souvenirs or gifts for your beer-loving friends and family. Everyone looks great in orange!
-          </p>
-          <p class="py-2">
-            Although our brewery does not have a kitchen, we invite you to order a pizza to your table, bring your own
-            picnic or
-            <a
-              v-scroll-to="'#food'"
-              href="#"
-            >
-              click here
-            </a>
-            to check out the tasty snacks we serve in our tasting room, all thoughtfully selected to go perfectly with
-            beer!
-          </p>
-          <p class="py-2">
-            Drop in for a pint, a growler fill or some take away cans. You know you want to!
-          </p>
+        <div class="w-1/2 inline">
+          <div class="tasting-room text-justify text-cbblack-grey font-semibold font-cbsans">
+            <p class="py-2">
+              Our brewery has a bright and family-friendly tasting room, a retail area and small patio, open 6 days a
+              week
+              and located next to the Safeway on Railway Avenue.
+            </p>
+            <p class="py-2">
+              In addition to our usual line up of beers, we also serve seasonal brews, pilot brews (our brewer’s latest
+              small batch creations), and our brand new Beer Cocktails. Juice and San Pellegrino are in the self-serve
+              case for designated drivers and youngsters.
+            </p>
+            <p class="py-2">
+              The retail area carries Canmore Brewing branded road cycling kit, mountain bike jerseys, an assortment of
+              tee shirts and flannels, hats, buffs, glassware and a rotating inventory of fun socks and novelty items.
+              Perfect as souvenirs or gifts for your beer-loving friends and family. Everyone looks great in orange!
+            </p>
+            <p class="py-2">
+              Although our brewery does not have a kitchen, we invite you to order a pizza to your table, bring your own
+              picnic or
+              <a
+                v-scroll-to="'#food'"
+                href="#"
+              >
+                click here
+              </a>
+              to check out the tasty snacks we serve in our tasting room, all thoughtfully selected to go perfectly with
+              beer!
+            </p>
+            <p class="py-2">
+              Drop in for a pint, a growler fill or some take away cans. You know you want to!
+            </p>
 
-          <p class="py-2">
-            <a
-              v-scroll-to="'#contact'"
-              href="#"
-            >Click here to view our summer hours</a>.
-          </p>
+            <p class="py-2">
+              <a
+                v-scroll-to="'#contact'"
+                href="#"
+              >Click here to view our summer hours</a>.
+            </p>
+          </div>
         </div>
-        <figure>
-          <picture>
-            <source
-              srcset="../../assets/img/tasting_room/taps-from-front.webp"
-              type="image/webp"
-            >
-            <source
-              srcset="../../assets/img/tasting_room/taps-from-front.jpg"
-              type="image/jpg"
-            >
-            <img
-              alt="Tasting Room"
-              src="../../assets/img/tasting_room/taps-from-front.jpg"
-            >
-          </picture>
-        </figure>
+        <div class="w-1/2 inline">
+          <figure>
+            <picture>
+              <source
+                srcset="../../assets/img/tasting_room/taps-from-front.webp"
+                type="image/webp"
+              >
+              <source
+                srcset="../../assets/img/tasting_room/taps-from-front.jpg"
+                type="image/jpg"
+              >
+              <img
+                alt="Tasting Room"
+                src="../../assets/img/tasting_room/taps-from-front.jpg"
+              >
+            </picture>
+          </figure>
+        </div>
       </div>
     </div>
   </section>
