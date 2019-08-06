ecf6d35fa2c6d1c57931369c5498d5c8ae811dcd
redxtech <gabe@gabedunn.dev>
2019-08-05 19:49:42 -0600

    add event component
    Former-commit-id: cc180f7717901e7b93b6955025ecbd40af3ab7d7 [formerly 3c214b467cefef88cf60e16edb99d0b0a74c7fdb] [formerly 58c28c92a4d1fd058c05f7e852786752188642de [formerly 6b697c09c0119fb94142edb7cd704be09cdea72a]]
    Former-commit-id: 6d88c4b1dc6848325d5c7ea3c08479c1924b04fe [formerly fe3a1be848196a0dd4ec81452bd62a728b7cce37]
    Former-commit-id: 34b0bf9aca604a84d882cf2ac59f696dfb50fbeb

---
+++ src/components/Event.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="flex flex-col items-stretch max-w-xs rounded overflow-hidden shadow-md">
+    <img
+      alt="Sunset in the mountains"
+      class="w-full"
+      src="https://tailwindcss.com/img/card-top.jpg"
+    >
+    <div class="flex-grow px-6 py-4">
+      <div
+        class="font-bold text-xl mb-2"
+        v-text="name"
+      />
+      <p
+        class="text-cbblack-grey text-base"
+        v-text="description"
+      />
+    </div>
+    <div class="px-6 py-4">
+      <span
+        class="inline-block bg-cborange rounded-full px-3 py-1 text-sm font-semibold text-white mr-2"
+        v-text="date"
+      />
+      <span
+        class="inline-block bg-cborange rounded-full px-3 py-1 text-sm font-semibold text-white mr-2"
+        v-text="time"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Event',
+    props: {
+      name: {
+        required: true,
+        type: String
+      },
+      date: {
+        required: true,
+        type: String
+      },
+      time: {
+        required: true,
+        type: String
+      },
+      description: {
+        required: true,
+        type: String
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  /* noinspection CssInvalidAtRule */
+  @tailwind base;
+  /* noinspection CssInvalidAtRule */
+  @tailwind components;
+  /* noinspection CssInvalidAtRule */
+  @tailwind utilities;
+</style>
