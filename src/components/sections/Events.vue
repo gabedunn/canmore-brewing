5f44c5c3a65011ef0f28d3d9199338d7124c9622
redxtech <gabe@gabedunn.dev>
2019-08-05 19:50:22 -0600

    make events how it is supposed to be
    Former-commit-id: 7a2cb80b3d933af80953c47d4daca178154bcb9b [formerly be65d9cf5d79648c8c65246399cd15c9ce51bc2b] [formerly a18a48f5d743e7277aa9847d4a722edc2b544428 [formerly 8186f0a7ec35bdd58c861159cdfe25d2e559f389]]
    Former-commit-id: c1eaa60cf0ccee8946a25f7989d8f027b21b206c [formerly 8b927342de2e9a5098dd77d2f6a01aef30870540]
    Former-commit-id: d9d0ddb19261a2640388b877b487041e7f8a533a

--- src/components/sections/Events.vue
+++ src/components/sections/Events.vue
@@ -1,36 +1,18 @@
 <template>
   <section id="events">
-    <header-title title="events" />
+    <header-title title="events"/>
     <div class="container">
       <div class="mx-40 my-12">
-        <div class="tasting-room text-justify text-cbblack-grey font-semibold font-cbsans">
-          <p>
-            We are super proud of our beautiful brewery and love to show it off. Book a tour to learn more about
-            where we came from and how we make our delicious beer.
-          </p>
-          <p>
-            Regular tours are available every Saturday from 1:30-2:30. Click the button below to reserve your
-            spot. Private
-            tours for 10 or more and private events are also available. Come celebrate with us. For more info
-            please contact.
-          </p>
+        <div class="flex flex-wrap items-stretch justify-around">
+          <event
+            v-for="event in events"
+            :date="event.date"
+            :description="event.description"
+            :key="event.name"
+            :name="event.name"
+            :time="event.time"
+          />
         </div>
-        <figure>
-          <picture>
-            <source
-              srcset="../../assets/img/tasting_room/landing-background.webp"
-              type="image/webp"
-            >
-            <source
-              srcset="../../assets/img/tasting_room/landing-background.png"
-              type="image/png"
-            >
-            <img
-              alt="Tasting Room"
-              src="../../assets/img/tasting_room/landing-background.png"
-            >
-          </picture>
-        </figure>
       </div>
     </div>
   </section>
@@ -38,11 +20,40 @@
 
 <script>
   import HeaderTitle from '../Header'
+  import Event from '../Event'
 
   export default {
     name: 'Events',
     components: {
+      Event,
       HeaderTitle
+    },
+    data () {
+      return {
+        events: [
+          {
+            name: 'Backlot Bash',
+            date: 'August 24, 2019',
+            time: '7:00 PM',
+            description: 'A great brewery party with half price craft beer, two bands, a grilled-cheese truck, and ' +
+              'much more! Come and join us!'
+          },
+          {
+            name: 'Event #2',
+            date: 'August 31, 2019',
+            time: '8:00 PM',
+            description: 'This is a filler event for the time being. It has no real meaning and if you can see this ' +
+              'I\'ve messed up.'
+          },
+          {
+            name: 'Event #3',
+            date: 'September 3, 2019',
+            time: '5:00 PM',
+            description: 'Yet another fake event that is not supposed to ever be public facing. This should not be ' +
+              'seen by anyone other than myself.'
+          }
+        ]
+      }
     }
   }
 </script>
