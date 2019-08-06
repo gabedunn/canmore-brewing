7cb68c1b7d1ab37f0eabba6b23cd83ebe949fa37
redxtech <gabe@gabedunn.dev>
2019-08-06 10:29:24 -0600

    move our story to tailwind
    Former-commit-id: 3593bddcc9844a249e7a2d80f524a1a58e98cff6 [formerly 0b266dfc01ac24e0f987388456c2363c1e059c32] [formerly bdbe64d32aff9abcc459df5474e1c4633ce57703 [formerly fe80e5038b86e864547e32356cc58f296ce13dad]]
    Former-commit-id: 6acca0a3c0ba4111840636ec74dc5db8b55c8e33 [formerly 76d82ac8b7d7649c2d85a1ec796324fc820542b8]
    Former-commit-id: 78e05fffbae7cb3474ef014aea831480f8d9e769

--- src/components/sections/OurStory.vue
+++ src/components/sections/OurStory.vue
@@ -1,58 +1,48 @@
 <template>
   <section id="our-story">
-    <div class="hero">
-      <div class="hero-body">
-        <div class="container">
-          <div class="columns">
-            <div class="column is-5 is-offset-1">
-              <div class="story">
-                <h1 class="title is-uppercase">
-                  Our Story
-                </h1>
-                <p class="has-text-justified">
-                  Growing up on the east coast, near the water, we learned how to enjoy the outdoors, have fun, build
-                  stuff, and do things the right way. The ocean was our playground. And we learned how to share with
-                  family, friends, and strangers too. We learned respect. The mountains, rivers and lakes of the west
-                  are
-                  our playgrounds now; go out and enjoy them. Share the times and stories with friends, family and
-                  strangers too. Over a beer. And respect them all.
-                </p>
-              </div>
-            </div>
-            <div class="column is-5 is-offset-1">
-              <figure class="image is-3by2">
-                <picture>
-                  <source
-                    srcset="../../assets/img/tasting_room/susie_at_taps.webp"
-                    type="image/webp" >
-                  <source
-                    srcset="../../assets/img/tasting_room/susie_at_taps.png"
-                    type="image/png" >
-                  <img
-                    src="../../assets/img/tasting_room/susie_at_taps.png"
-                    alt="Susie at the taps"
-                  >
-                </picture>
-              </figure>
-            </div>
-          </div>
-          <div class="columns">
-            <div class="column is-offset-1">
-              <div class="values has-padding-top-lg">
-                <h1 class="title is-uppercase">
-                  Our Core Values
-                </h1>
-                <ul class="has-padding-left-lg">
-                  <li>People come first.</li>
-                  <li>Respect it. Whether it’s a person, tradition, our products, or the planet.</li>
-                  <li>Be a positive part of the community.</li>
-                  <li>Our compass is driven by honesty and truthfulness to ourselves and our customers</li>
-                  <li>Respectful and positive teams focused on “we” not “I”.</li>
-                  <li>We will work hard to make our customers successful. We’re nothing without them.</li>
-                </ul>
-              </div>
-            </div>
-          </div>
+    <div class="container">
+      <div class="mx-8 lg:mx-30 py-10 flex flex-wrap justify-between">
+        <div class="lg:w-1/2">
+          <h1 class="title uppercase">
+            Our Story
+          </h1>
+          <p class="story text-justify">
+            Growing up on the east coast, near the water, we learned how to enjoy the outdoors, have fun, build
+            stuff, and do things the right way. The ocean was our playground. And we learned how to share with
+            family, friends, and strangers too. We learned respect. The mountains, rivers and lakes of the west
+            are
+            our playgrounds now; go out and enjoy them. Share the times and stories with friends, family and
+            strangers too. Over a beer. And respect them all.
+          </p>
+        </div>
+        <div class="lg:w-5/12 pt-12 lg:pt-0">
+          <figure>
+            <picture>
+              <source
+                srcset="../../assets/img/tasting_room/susie_at_taps.webp"
+                type="image/webp">
+              <source
+                srcset="../../assets/img/tasting_room/susie_at_taps.png"
+                type="image/png">
+              <img
+                alt="Susie at the taps"
+                src="../../assets/img/tasting_room/susie_at_taps.png"
+              >
+            </picture>
+          </figure>
+        </div>
+        <div class="w-full pt-12">
+          <h1 class="title is-uppercase">
+            Our Core Values
+          </h1>
+          <ul class="values list-disc ml-6 md:ml-8 lg:ml-10 fhd:ml-12">
+            <li>People come first.</li>
+            <li>Respect it. Whether it’s a person, tradition, our products, or the planet.</li>
+            <li>Be a positive part of the community.</li>
+            <li>Our compass is driven by honesty and truthfulness to ourselves and our customers</li>
+            <li>Respectful and positive teams focused on “we” not “I”.</li>
+            <li>We will work hard to make our customers successful. We’re nothing without them.</li>
+          </ul>
         </div>
       </div>
     </div>
@@ -60,74 +50,67 @@
 </template>
 
 <script>
-  // TODO: switch to tailwind
-
   export default {
     name: 'OurStory'
   }
 </script>
 
-<style scoped lang="scss">
-  $border: 0;
-  @import "../../../node_modules/bulma/sass/utilities/mixins";
+<style scoped>
+  @import "../../assets/css/tailwind.css";
 
   .title {
-    font-family: "Archivo Black", Arial, sans-serif;
+    @apply font-cbtitle;
     font-size: 1.7rem;
-    @include tablet() {
-      font-size: 2rem;
-    }
-    @include desktop() {
-      font-size: 2.5rem;
-    }
-    @include widescreen() {
-      font-size: 3.2rem;
-    }
-    @include fullhd() {
-      font-size: 4.2rem;
-    }
   }
 
   .story {
-    p {
-      font-family: "Open Sans", Arial, sans-serif;
-      font-weight: 600;
-      font-size: 1rem;
-      @include tablet() {
-        font-size: 1.1rem;
-      }
-      @include desktop() {
-        font-size: 1.2rem;
-      }
-      @include widescreen() {
-        font-size: 1.4rem;
-      }
-      @include fullhd() {
-        font-size: 1.6rem;
-      }
+    @apply font-cbsans font-semibold;
+    font-size: 1rem;
+  }
+
+  .values li {
+    font-family: "Open Sans", Arial, sans-serif;
+    font-weight: 600;
+    font-size: 1rem;
+  }
+
+  @media (min-width: 768px) {
+    .title {
+      font-size: 2rem;
+    }
+
+    .story, .values li {
+      font-size: 1.1rem;
     }
   }
 
-  .values {
-    ul {
-      list-style: disc;
-      li {
-        font-family: "Open Sans", Arial, sans-serif;
-        font-weight: 600;
-        font-size: 1rem;
-        @include tablet() {
-          font-size: 1.1rem;
-        }
-        @include desktop() {
-          font-size: 1.2rem;
-        }
-        @include widescreen() {
-          font-size: 1.4rem;
-        }
-        @include fullhd() {
-          font-size: 1.6rem;
-        }
-      }
+  @media (min-width: 1024px) {
+    .title {
+      font-size: 2.5rem;
+    }
+
+    .story, .values li {
+      font-size: 1.2rem;
+    }
+  }
+
+  @media (min-width: 1280px) {
+    .title {
+      font-size: 3.32rem;
+    }
+
+    .story, .values li {
+      font-size: 1.4rem;
+    }
+  }
+
+  @media (min-width: 1440px) {
+    .title {
+      font-size: 4.2rem;
+    }
+
+    .story, .values li {
+      font-size: 1.6rem;
     }
   }
 </style>
