c03d96e395f5ed489c28296c904977a5655d6e30
redxtech <gabe@gabedunn.dev>
2019-08-07 22:35:22 -0600

    do events section properly
    Former-commit-id: aadc89335054f67246c0a5a9642854fa0effd5bc [formerly 7c723ecaaaedd9d7f3a16e11f10dc0c85ec4302c] [formerly 2eb79f24f299ab0edfeb02d51290484cfd1253ec [formerly 8d8e4847c30da646cd57d851b73732d5fdcfe523]]
    Former-commit-id: 76ae9115750e932e849faa3d41dd44862cac889e [formerly 70d8b30e065959545742767cdef2c50f7bdbe10f]
    Former-commit-id: 05a72feef57f8dab76d85fe30a0e11fd0d705772

--- src/components/sections/Events.vue
+++ src/components/sections/Events.vue
@@ -2,17 +2,26 @@
   <section id="events">
     <header-title title="events"/>
     <div class="container">
-      <div class="mx-8 lg:mx-40 my-12">
-        <div class="flex flex-wrap items-stretch justify-between">
-          <event
-            v-for="event in events"
-            :date="event.date"
-            :description="event.description"
-            :key="event.name"
-            :name="event.name"
-            :time="event.time"
-          />
-        </div>
+      <div class="mx-8 lg:mx-40 my-12 text-justify">
+        <h1 class="title uppercase">Need a Space?</h1>
+        <p class="py-2">
+          Having a get-together and looking for something a little more exciting than a conference room? Get in touch
+          with us to see if we have a time or space that fits the bill. We host a variety of events from a small
+          gathering at a few tables to a private function where you can have the place to yourself. Between the taproom
+          and brewhouse seating areas we can accommodate up to 140 guests. Have the event catered by your favorite chef
+          or bring your own. We’ve got the drinks covered!
+        </p>
+        <p class="py-2">
+          For rates, availability and more details on just how flexible we can be, send a note to
+          <a href="mailto:susan.dunn@canmorebrewing.com?subject=Booking Information">susan.dunn@canmorebrewing.com</a>.
+        </p>
+        <h1 class="title uppercase pt-8">Upcoming Events</h1>
+        <p class="py-2">
+          Check our social media streams here to see what’s coming up at the brewery. From our Brewhouse Concert Series,
+          new beer launches, cheap beer nights, fundraisers, movie nights, or an assortment of other great events, we’ve
+          probably got something going on that you should pencil into your calendar, or get Siri to do it, or Alexa, or
+          however the kids do it these days.
+        </p>
       </div>
     </div>
   </section>
@@ -20,45 +29,65 @@
 
 <script>
   import HeaderTitle from '../Header'
-  import Event from '../Event'
 
   export default {
     name: 'Events',
     components: {
-      Event,
       HeaderTitle
-    },
-    data () {
-      return {
-        // TODO: update events. maybe pull from contentful?
-        events: [
-          {
-            name: 'Backlot Bash',
-            date: 'August 24, 2019',
-            time: '7:00 PM',
-            description: 'A great brewery party with half price craft beer, two bands, a grilled-cheese truck, and ' +
-              'much more! Come and join us!'
-          },
-          {
-            name: 'Event #2',
-            date: 'August 31, 2019',
-            time: '8:00 PM',
-            description: 'This is a filler event for the time being. It has no real meaning and if you can see this ' +
-              'I\'ve messed up.'
-          },
-          {
-            name: 'Event #3',
-            date: 'September 3, 2019',
-            time: '5:00 PM',
-            description: 'Yet another fake event that is not supposed to ever be public facing. This should not be ' +
-              'seen by anyone other than myself.'
-          }
-        ]
-      }
     }
   }
 </script>
 
 <style scoped>
   @import "../../assets/css/tailwind.css";
+
+  .title {
+    @apply font-cbtitle;
+    font-size: 1.7rem;
+  }
+
+  p {
+    @apply font-cbsans font-semibold;
+    font-size: 1rem;
+  }
+
+  @media (min-width: 768px) {
+    .title {
+      font-size: 2rem;
+    }
+
+    p {
+      font-size: 1.1rem;
+    }
+  }
+
+  @media (min-width: 1024px) {
+    .title {
+      font-size: 2.5rem;
+    }
+
+    p {
+      font-size: 1.2rem;
+    }
+  }
+
+  @media (min-width: 1280px) {
+    .title {
+      font-size: 3.32rem;
+    }
+
+    p {
+      font-size: 1.4rem;
+    }
+  }
+
+  @media (min-width: 1440px) {
+    .title {
+      font-size: 4.2rem;
+    }
+
+    p {
+      font-size: 1.6rem;
+    }
+  }
 </style>
