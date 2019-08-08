3e4ecd17ecb3c8e0789131eca2a1917480e44386
redxtech <gabe@gabedunn.dev>
2019-08-08 15:11:57 -0600

    move navbar to tailwind

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav
-    class="navbar is-fixed-top is-cb-orange"
+    class="navbar is-fixed-top is-cborange"
     role="navigation"
     aria-label="main navigation">
     <div class="container">
@@ -13,10 +13,10 @@
           <picture>
             <source
               srcset="../assets/img/CanmoreBrewingLogo.webp"
-              type="image/webp" >
+              type="image/webp">
             <source
               srcset="../assets/img/CanmoreBrewingLogo.png"
-              type="image/png" >
+              type="image/png">
             <img
               alt="Canmore Brewing Logo"
               src="../assets/img/CanmoreBrewingLogo.png"
@@ -27,9 +27,9 @@
           class="button navbar-burger"
           data-target="nav-menu"
         >
-          <span />
-          <span />
-          <span />
+          <span/>
+          <span/>
+          <span/>
         </button>
       </div>
       <div
@@ -134,25 +134,212 @@
   }
 </script>
 
-<style scoped lang="scss">
-  nav {
-    .navbar-brand {
-      a.navbar-item {
-        padding-top: 0;
-        padding-bottom: 0;
-      }
+<style scoped>
+  @import "../assets/css/tailwind.css";
+
+  .navbar {
+    background-color: white;
+    min-height: 4.5rem;
+    position: relative;
+    z-index: 30;
+  }
+
+  .navbar.is-cborange {
+    background-color: #F45A24;
+    color: #fff;
+  }
+
+  .navbar.is-cborange .navbar-brand > .navbar-item:hover {
+    background-color: #f2480c;
+  }
+
+  .navbar.is-cborange .navbar-burger {
+    color: #fff;
+  }
+
+  @media screen and (min-width: 1088px) {
+    .navbar.is-cborange .navbar-end > .navbar-item {
+      color: #fff;
+    }
+
+    .navbar.is-cborange .navbar-end > .navbar-item:hover {
+      background-color: #f2480c;
+    }
+  }
+
+  .navbar > .container {
+    align-items: stretch;
+    display: flex;
+    min-height: 4.5rem;
+    width: 100%;
+  }
+
+  .navbar.is-fixed-top {
+    left: 0;
+    position: fixed;
+    right: 0;
+    z-index: 30;
+    top: 0;
+  }
+
+  .navbar-brand {
+    align-items: stretch;
+    display: flex;
+    flex-shrink: 0;
+    min-height: 4.5rem;
+  }
+
+  .navbar-burger {
+    color: #4a4a4a;
+    cursor: pointer;
+    display: block;
+    height: 4.5rem;
+    position: relative;
+    width: 4.5rem;
+    margin-left: auto;
+  }
+
+  .navbar-burger span {
+    background-color: currentColor;
+    display: block;
+    height: 1px;
+    left: calc(50% - 8px);
+    position: absolute;
+    transform-origin: center;
+    transition-duration: 86ms;
+    transition-property: background-color, opacity, transform;
+    transition-timing-function: ease-out;
+    width: 16px;
+  }
+
+  .navbar-burger span:nth-child(1) {
+    top: calc(50% - 6px);
+  }
+
+  .navbar-burger span:nth-child(2) {
+    top: calc(50% - 1px);
+  }
+
+  .navbar-burger span:nth-child(3) {
+    top: calc(50% + 4px);
+  }
+
+  .navbar-burger:hover {
+    background-color: rgba(0, 0, 0, 0.05);
+  }
+
+  .navbar-burger.is-active span:nth-child(1) {
+    transform: translateY(5px) rotate(45deg);
+  }
+
+  .navbar-burger.is-active span:nth-child(2) {
+    opacity: 0;
+  }
+
+  .navbar-burger.is-active span:nth-child(3) {
+    transform: translateY(-5px) rotate(-45deg);
+  }
+
+  .navbar-menu {
+    display: none;
+  }
+
+  .navbar-item {
+    color: #4a4a4a;
+    display: block;
+    line-height: 1.5;
+    padding: 0.5rem 0.75rem;
+    position: relative;
+    cursor: pointer;
+    flex-grow: 0;
+    flex-shrink: 0;
+  }
+
+  .navbar-item:hover {
+    background-color: #fafafa;
+    color: #3273dc;
+  }
+
+  .navbar-item img {
+    max-height: 4.25rem;
+  }
+
+  @media screen and (max-width: 1087px) {
+    .navbar > .container {
+      display: block;
+    }
+
+    .navbar-brand .navbar-item {
+      align-items: center;
+      display: flex;
     }
 
     .navbar-menu {
-      a.navbar-item {
-        font-size: 1.2rem;
-        /*font-weight: bold;*/
-      }
+      background-color: white;
+      box-shadow: 0 8px 16px rgba(10, 10, 10, 0.1);
+      padding: 0.5rem 0;
+    }
+
+    .navbar-menu.is-active {
+      display: block;
+    }
+
+    .navbar.is-fixed-top .navbar-menu {
+      -webkit-overflow-scrolling: touch;
+      max-height: calc(100vh - 4.5rem);
+      overflow: auto;
+    }
+  }
+
+  @media screen and (min-width: 1088px) {
+    .navbar,
+    .navbar-menu,
+    .navbar-end {
+      align-items: stretch;
+      display: flex;
+    }
+
+    .navbar {
+      min-height: 4.5rem;
     }
 
     .navbar-burger {
-      border: none;
-      background-color: rgba(0, 0, 0, 0);
+      display: none;
     }
+
+    .navbar-item {
+      align-items: center;
+      display: flex;
+    }
+
+    .navbar-menu {
+      flex-grow: 1;
+      flex-shrink: 0;
+    }
+
+    .navbar-end {
+      justify-content: flex-end;
+      margin-left: auto;
+    }
+
+    .navbar > .container .navbar-brand {
+      margin-left: -0.75rem;
+    }
+
+    .navbar > .container .navbar-menu {
+      margin-right: -0.75rem;
+    }
+  }
+
+  .navbar-brand a.navbar-item {
+    @apply py-0;
+  }
+
+  .navbar-menu a.navbar-item {
+    @apply text-xl;
+  }
+
+  .navbar-burger {
+    @apply bg-transparent border-none;
   }
 </style>
