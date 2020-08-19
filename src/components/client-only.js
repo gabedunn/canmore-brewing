a27fe555ea0ffbfa9fb73ee03ff7979db9f46051
Gabe Dunn <gabe.dunn@shaw.ca>
2020-08-19 00:01:01 -0600

    add client-only component

---
+++ src/components/client-only.js
@@ -0,0 +1,41 @@
+export default {
+  name: 'ClientOnly',
+  functional: true,
+  props: {
+    placeholder: String,
+    placeholderTag: {
+      type: String,
+      default: 'div'
+    }
+  },
+  render (h, { parent, slots, props }) {
+    const { default: defaultSlot = [], placeholder: placeholderSlot } = slots()
+
+    const isServerRendered =
+      !parent._isMounted || // default detection
+      !!window.__PRERENDER_STATUS || // prerender-spa-plugin
+      navigator.userAgent === 'ReactSnap' // react-snap
+
+    if (!isServerRendered) {
+      return defaultSlot
+    }
+
+    parent.$once('hook:mounted', () => {
+      parent.$forceUpdate()
+    })
+
+    if (props.placeholderTag && (props.placeholder || placeholderSlot)) {
+      return h(
+        props.placeholderTag,
+        {
+          class: ['client-only-placeholder']
+        },
+        props.placeholder || placeholderSlot
+      )
+    }
+
+    // Return a placeholder element for each child in the default slot
+    // Or if no children return a single placeholder
+    return defaultSlot.length > 0 ? defaultSlot.map(() => h(false)) : h(false)
+  }
+}
